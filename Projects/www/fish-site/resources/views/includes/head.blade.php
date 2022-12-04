9e18cc3475b7162278fe8b315194449ea650e51c
Akuma <74576692+MarkFidrus@users.noreply.github.com>
2022-12-04 17:29:04 +0100

    FS - public/less - Important less files has been refactored for generating .css and .css.map files to .less.

--- Projects/www/fish-site/resources/views/includes/head.blade.php
+++ Projects/www/fish-site/resources/views/includes/head.blade.php
@@ -4,10 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="{{ asset('less/header.css') }}" type="text/css">
+    <link rel="stylesheet" href="{{ asset('less/includes/header.css') }}" type="text/css">
     <link rel="stylesheet" href="{{ asset('less/'. $less.'') }}" type="text/css">
-    <link rel="stylesheet" href="{{ asset('less/alertMessage.css') }}" type="text/css">
-    <link rel="stylesheet" href="{{ asset('less/footer.css') }}" type="text/css">
-    <link rel="stylesheet" href="{{ asset('less/toTop.css') }}" type="text/css">
+    <link rel="stylesheet" href="{{ asset('less/includes/alertMessage.css') }}" type="text/css">
+    <link rel="stylesheet" href="{{ asset('less/includes/footer.css') }}" type="text/css">
+    <link rel="stylesheet" href="{{ asset('less/includes/toTop.css') }}" type="text/css">
     <title>LP - {{ $title }}</title>
 </head>
