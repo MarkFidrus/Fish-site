5f9d046e19bbfdb64c37a4a01d6f66d12e5d04d4
Akuma <74576692+MarkFidrus@users.noreply.github.com>
2022-12-04 16:44:34 +0100

    FS - resources/views/includes & resources/views/layouts - Head.blade.php has been finished. App.blade.php has been finished.

---
+++ Projects/www/fish-site/resources/views/includes/head.blade.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('css/header.css') }}" type="text/css">
+    <link rel="stylesheet" href="{{ asset('css/'. $cssName.'') }}" type="text/css">
+    <link rel="stylesheet" href="{{ asset('css/alertMessage.css') }}" type="text/css">
+    <link rel="stylesheet" href="{{ asset('css/footer.css') }}" type="text/css">
+    <link rel="stylesheet" href="{{ asset('css/toTop.css') }}" type="text/css">
+    <title>LP - {{ $title }}</title>
+</head>
