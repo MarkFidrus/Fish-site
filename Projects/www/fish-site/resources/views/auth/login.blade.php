da7976973a748578a4b9fa0c0f470196d5d15c15
Akuma <74576692+MarkFidrus@users.noreply.github.com>
2022-12-04 16:41:35 +0100

    FS - resources/views/auth & resources/less/auth - Login.blade.php, register.blade.php has been finished. Login.less, registration.less has been finished.

--- Projects/www/fish-site/resources/views/auth/login.blade.php
+++ Projects/www/fish-site/resources/views/auth/login.blade.php
@@ -1,73 +1,60 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
-
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
+    <section class="login__container">
+        <div class="login__container-bg__video__container">
+            <video class="login__container-bg__video__container-video" autoplay muted loop>
+                <source src="/vids/bgvideo.mp4" type="video/mp4">
+            </video>
         </div>
-    </div>
-</div>
+        <div class="login__container-video__effect"></div>
+        <div class="login__container-content">
+            <form method="POST" action="{{ route('login') }}">
+                @csrf
+                <div class="login__container-content-header">
+                    <h1 class="login__container-content-header-title">{{ __('Login') }}</h1>
+                </div>
+                <div class="login__container-content-body">
+                    <div class="login__container-content-body-email__field">
+                        <input id="email" type="email" class="login__container-content-body-email__field-input" name="email" value="{{ old('email') }}" required autocomplete="email" placeholder="Your email address">
+                        <i class="login__container-content-body-email__field-icon fa-solid fa-envelope"></i>
+                        <label class="login__container-content-body-email__field-label" for="email">{{ __('Email Address') }}</label>
+                    </div>
+                    <div class="login__container-content-body-password__field">
+                        <input id="password" type="password" class="login__container-content-body-password__field-input" name="password" required autocomplete="current-password" placeholder="Your password">
+                        <i class="login__container-content-body-password__field-icon fa-solid fa-lock"></i>
+                        <label for="password" class="login__container-content-body-password__field-label">{{ __('Password') }}</label>
+                    </div>
+                    <div class="login__container-content-body-btn__field">
+                        <button type="submit" class="login__container-content-body-btn__field-btn">{{ __('Login') }}</button>
+                    </div>
+                </div>
+                <div class="login__container-content-footer">
+                    <div class="login__container-content-footer-row__1">
+                        <div class="login__container-content-footer-row__1-remember__field">
+                            <label class="login__container-content-footer-row__1-remember__field-label__1">
+                                <input class="login__container-content-footer-row__1-remember__field-label__1-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                                <div class="login__container-content-footer-row__1-remember__field-label__1-slider">
+                                    <div class="login__container-content-footer-row__1-remember__field-label__1-slider-knob"></div>
+                                </div>
+                            </label>
+
+                            <label class="login__container-content-footer-row__1-remember__field-label__2" for="remember">{{ __('Remember Me') }}</label></div>
+                        <div class="login__container-content-footer-row__1-reset__password__field">
+                            @if (Route::has('password.request'))
+                                <a class="login__container-content-footer-row__1-reset__password__field-link" href="{{ route('password.request') }}">
+                                    {{ __('Forgot Password ?') }}
+                                </a>
+                            @endif
+                        </div>
+
+                    </div>
+                    <div class="login__container-content-footer-row__2">
+                        <p class="login__container-content-footer-row__2-text">{{__("Don't have an account ?")}}</p>
+                        <a class="login__container-content-footer-row__2-link" href="/register">{{ __('Create account') }}<i class="login__container-content-footer-row__2-link-icon fa-solid fa-hand-point-left"></i></a>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </section>
 @endsection
